52f5fb6ba1725dfcc9201e85340e45e48205a127
Lebibi <lebihannathan@gmail.com>
2021-02-02 01:57:54 +0100

    conv

--- printf_convert.c
+++ printf_convert.c
@@ -6,42 +6,18 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 01:45:48 by nle-biha          #+#    #+#             */
-/*   Updated: 2021/02/02 01:46:05 by nle-biha         ###   ########.fr       */
+/*   Updated: 2021/02/02 01:57:45 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-
-char	*ft_utoa_base(unsigned int nbr, char *base, char *returned)
-{
-	long int	size;
-	long int	nb;
-	char		*temp;
-	long int	retsize;
-	
-	nb = nbr;
-	size = ft_strlen(base);
-	if (nb >= size)
-		if (!(returned = ft_utoa_base((unsigned int)(nb / size), base, returned)))
-			return (NULL);
-	retsize = ft_strlen(returned);
-	if (!(temp = malloc(retsize + 2)))
-	{
-		free(returned);
-		return (NULL);
-	}
-	ft_strcpy(temp, returned);
-	temp[retsize] = base[nb % size];
-	temp[retsize + 1] = '\0';
-	return (temp);
-}
 
 char	*neg_initial(long int *nb, char *returned)
 {
 	if (*nb < 0)
 	{
 		free(returned);
-		returned = malloc(2);
+		returned = (char *)malloc(2 * sizeof(char));
 		returned[0] = '-';
 		returned[1] = '\0';
 		*nb = *nb * (-1);
@@ -49,26 +25,25 @@ char	*neg_initial(long int *nb, char *returned)
 	return (returned);
 }
 
-char	*ft_itoa_base(int nbr, char *base, char *returned)
+char	*ft_litoa_base(long int nb, char *base, char *returned)
 {
 	long int	size;
-	long int	nb;
 	char		*temp;
 	long int	retsize;
 	
-	nb = nbr;
 	size = ft_strlen(base);
 	neg_initial(&nb, returned);
 	if (nb >= size)
-		if (!(returned = ft_utoa_base((int)(nb / size), base, returned)))
+		if (!(returned = ft_litoa_base((nb / size), base, returned)))
 			return (NULL);
 	retsize = ft_strlen(returned);
-	if (!(temp = malloc(retsize + 2)))
+	if (!(temp = (char *)malloc((retsize + 2) * sizeof(char))))
 	{
 		free(returned);
 		return (NULL);
 	}
 	ft_strcpy(temp, returned);
+	free(returned);
 	temp[retsize] = base[nb % size];
 	temp[retsize + 1] = '\0';
 	return (temp);
--- printf_utils.c
+++ printf_utils.c
@@ -6,11 +6,11 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 01:43:03 by nle-biha          #+#    #+#             */
-/*   Updated: 2021/02/02 01:48:02 by nle-biha         ###   ########.fr       */
+/*   Updated: 2021/02/02 01:54:45 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy_freesrc(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src)
 {
 	int i;
 
@@ -21,7 +21,6 @@ char	*ft_strcpy_freesrc(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-	free(src);
 	return (dest);
 }
 
