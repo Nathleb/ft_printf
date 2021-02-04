73cf8a293740ce8347dca89cd8def3b6766d2eda
Lebibi <lebihannathan@gmail.com>
2021-02-04 02:46:47 +0100

    yes

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,17 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 19:57:18 by nle-biha          #+#    #+#             */
-/*   Updated: 2021/02/03 21:26:41 by nle-biha         ###   ########.fr       */
+/*   Updated: 2021/02/04 02:46:37 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+int  ft_pfdisplay_strings(t_flags flags,char * str)
+{	
+	printf("%s", str);
+	return (1);
+}
 
 int ft_pfdisplay(char *str, va_list ap)
 {
@@ -18,9 +24,10 @@ int ft_pfdisplay(char *str, va_list ap)
 	t_flags flags;
 
 	flags = printf_parse(str, ap);
-	if (flags.conv_type = )
-
 	free(str);
+	if (flags.conv_type == 's')
+		ft_pfdisplay_strings(flags, va_arg(ap, char *));
+
 	return (ret);
 }
 
@@ -44,7 +51,7 @@ int	ft_printf(const char *str, ...)
 			while (str[i] && !ft_isin("cspdiuxX%", str[i]))
 				i++;
 			if (ft_isin("cspdiuxX%", str[i]))
-				ret += ft_pfdisplay(ft_substr(str, j, i - j), ap);
+				ret += ft_pfdisplay(ft_substr(str, j, ++i - j), ap);
 		}
 	}
 	va_end(ap);
@@ -54,6 +61,7 @@ int	ft_printf(const char *str, ...)
 int main(int argc, char **argv)
 {
 	(void)argc;
-	ft_printf(argv[1]);
+	char * t = "test tut coco";
+	ft_printf(argv[1],t);
 	return 0;
 }
