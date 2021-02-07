65ac4ac02f6776dcb5906b418d6d7ea04dc790d4
Lebibi <lebihannathan@gmail.com>
2021-02-07 17:10:55 +0100

    fdfd

--- printf_display.c
+++ printf_display.c
@@ -6,21 +6,19 @@
 /*   By: nle-biha <nle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 20:56:22 by nle-biha          #+#    #+#             */
-/*   Updated: 2021/02/07 16:34:15 by nle-biha         ###   ########.fr       */
+/*   Updated: 2021/02/07 17:04:09 by nle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_putpad(t_flags flags, int pad_size)
+int	ft_putspace(t_flags flags, int pad_size)
 {
 	char c;
 	int ret;
 
 	ret = 0;
 	c = ' ';
-	if (flags.zero && !flags.left)
-		c = '0';
 	while (pad_size-- > 0)
 		ret += write(1, &c, 1);
 	return (ret);
@@ -33,20 +31,22 @@ int	ft_display_string(t_flags flags, char *s)
 	unsigned int len;
 
 	ret = 0;
-	if (s)
+	if (!s)
 	{
+		len = (flags.prec >= 0 && flags.prec <= 5) ? 0 : 6;
+		ret += ft_putspace(flags, flags.width - len);
+		ret += write(1, "(null)", len);
+		return (ret);
+	}
 		len = ft_strlen(s);
 		if (flags.prec == -1)
 			flags.prec = len;
 		pad_size = flags.width - ft_min(flags.prec, len);
 		if (!flags.left)
-			ret += ft_putpad(flags, pad_size);
+			ret += ft_putspace(flags, pad_size);
 		ret += write(1, s, ft_min(flags.prec, len));
 		if (flags.left)
-			ret += ft_putpad(flags, pad_size);
-		return (ret);
-	}
-	ret += write(1, "(null)", 6);
+			ret += ft_putspace(flags, pad_size);
 	return (ret);
 }
 
@@ -128,9 +128,9 @@ int	ft_display_char(t_flags flags, char c)
 		flags.prec = 1;
 	pad_size = flags.width - ft_min(flags.prec, 1);
 	if (flags.left)
-		ret += ft_putpad(flags, pad_size);
+		ret += ft_putspace(flags, pad_size);
 	ret += write(1, &c, ft_min(flags.prec, 1));
 	if (!flags.left)
-		ret += ft_putpad(flags,pad_size);
+		ret += ft_putspace(flags,pad_size);
 	return (ret);
 }
